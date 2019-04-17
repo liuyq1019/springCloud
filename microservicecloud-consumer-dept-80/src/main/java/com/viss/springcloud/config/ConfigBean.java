7cab4768dde83394959632df9e3a0c7c550561c6
liu.yongquan <liu.yongquan@zxelec.com>
2019-04-17 18:12:57 +0800

    1

--- microservicecloud-consumer-dept-80/src/main/java/com/viss/springcloud/config/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/viss/springcloud/config/ConfigBean.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ConfigBean {
+public class ConfigBean { 
 	//需要使用一个RestT面板
 	@Bean
-	@LoadBalanced //spring cloud Ribbon 基于Netfilx Ribbon实现的一套客户端 负载均衡工具
+	@LoadBalanced //spring   cloud Ribbon 基于Netfilx Ribbon实现的一套客户端 负载均衡工具
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
