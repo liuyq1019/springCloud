27fa613f9a26fa1610284c0d18247528d14f5f5f
liu.yongquan <liu.yongquan@zxelec.com>
2019-06-12 09:16:28 +0800

    1

--- microservicecloud-consumer-dept-80/src/main/java/com/viss/springcloud/config/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/viss/springcloud/config/ConfigBean.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
 @Configuration
 public class ConfigBean { 
 	//需要使用一个RestT面板
@@ -12,5 +15,11 @@ public class ConfigBean {
 	@LoadBalanced //spring   cloud Ribbon 基于Netfilx Ribbon实现的一套客户端 负载均衡工具
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	//自定义访问策略
+	@Bean
+	public IRule myRules() {
+		return new RandomRule();
 	}
 }
